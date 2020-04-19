9a582d0f3b5e373cc294e29940b7a7dedd740f47
anonymouspanda-23 <1702951c@student.tp.edu.sg>
2020-04-20 00:54:55 +0800

    Added new stuff

---
+++ public/scripts/social.js
@@ -0,0 +1,10 @@
+let confucius = document.getElementById("confucius");
+let positivity = document.getElementById("positivity");
+
+confucius.onclick = function() {
+    window.open("http://www.brainyquote.com/quotes/authors/c/confucius.html", "_blank");
+}
+
+positivity.onclick = function() {
+    window.open("http://www.affirmationsforpositivethinking.com/index.htm", "_blank");
+}
--- routes/main.py
+++ routes/main.py
@@ -1,5 +1,5 @@
 routes = {
-    "/": {
+    "/": {  # todo: add index to path
         "template": "index.html"
     },
     "/goodbye": {
@@ -13,5 +13,17 @@ routes = {
     },
     "/recipes/hummus": {
         "template": "hummus_recipe.html"
+    },
+    "/index": {
+        "template": "nav-tut/index.html"
+    },
+    "/projects": {
+        "template": "nav-tut/projects.html"
+    },
+    "/pictures": {
+        "template": "nav-tut/pictures.html"
+    },
+    "/social": {
+        "template": "nav-tut/social.html"
     }
 }
