b0b3b17b5a306ed3ae3af9d33827ba996f38647d
anonymouspanda-23 <1702951c@student.tp.edu.sg>
2020-04-21 00:36:35 +0800

    Added new stuff

--- public/scripts/social.js
+++ public/scripts/social.js
@@ -6,5 +6,21 @@ confucius.onclick = function() {
 }
 
 positivity.onclick = function() {
-    window.open("http://www.affirmationsforpositivethinking.com/index.htm", "_blank");
+    window.open("http://www.brainyquote.com/quotes/authors/c/confucius.html", "_blank");
 }
+
+//todo: fix left-click open and view in new tab but middle click open and not focused
+confucius.addEventListener('mousedown', function(event) {
+    console.log(event.button);
+    // todo: add checks for mouse configuration and check mouse button values based on config and browser
+    if (event.button == 1) {
+        window.open("http://www.brainyquote.com/quotes/authors/c/confucius.html");
+    }
+});
+
+positivity.addEventListener('mousedown', function(event) {
+    console.log(event.button);
+    if (event.button == 1) {
+        window.open("http://www.affirmationsforpositivethinking.com/index.htm", "_blank");
+    }
+});
